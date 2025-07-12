f2dbc0b1cab1d3dca8a692c426bec00c71238f3f
Afzal khan <afzalkhan01765@gmail.com>
2025-07-12 22:43:36 +0600

    Add files via upload

---
+++ fastinput_output.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a , b,sum_value;  // input of speedy gether then normal input system .
+
+    cin>>a>>b;
+    sum_value  = a + b ;
+    cout<<"sumation of two value is :"<<sum_value<<endl;
+
+    return 0;
+}
